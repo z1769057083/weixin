d7356cd5c607c6eac6a85ce8135268a6703b1bc6
z1769057083 <1769057083@qq.com>
2016-11-01 21:38:59 +0800

    第一次提交

---
+++ server/app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+//加载 token 模块
+require("./token")(app);
+var saticpath = path.join(__dirname,"../static");
+app.use(express.static(saticpath));
+app.listen(80,function(){
+	console.log("open http://127.0.0.1:80");
+})
