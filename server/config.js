b9d6c67e1e9058b1b1206fafe9d9564340bc08c7
z1769057083 <1769057083@qq.com>
2016-11-01 22:10:08 +0800

    第三次提交

---
+++ server/config.js
@@ -0,0 +1,13 @@
+ * 如果有需要可以将这些信息放到数据库中读取方便以更增加
+ * 注：测试时请替换您真实的数据
+ */
+module.exports = function(){
+	return {
+//		AppID(应用ID)
+		appid: 'wxd9f0d6cef42d2aca',
+		//AppSecret(应用密钥)
+		appsecret: '511a2efb1d25a530216a05355792514b',
+		//Token(令牌)
+		token: 'Tokenjs'
+	};
+};
